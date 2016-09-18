299104cf3c83ec9ddfff93381c8a667380a4186a
Mingqi Shao <shaomq@gmail.com>
2016-09-18 16:42:39 +0800

    ci

--- result/tests/tests.sh
+++ result/tests/tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 while ! timeout 1 bash -c "echo > /dev/tcp/vote/80"; do sleep 1; done
-curl -sS -X POST --data "vote=b" http://vote > /dev/null
+curl -sS -X POST --data "vote=b" http://vote.voting-claasv1.myalauda.cn/ > /dev/null
 sleep 10
-if phantomjs render.js http://result | grep -q '1 vote'; then
+if phantomjs render.js http://result.voting-claasv1.myalauda.cn/ | grep -q '1 vote'; then
   echo -e "\e[42m------------"
   echo -e "\e[92mTests passed"
   echo -e "\e[42m------------"
